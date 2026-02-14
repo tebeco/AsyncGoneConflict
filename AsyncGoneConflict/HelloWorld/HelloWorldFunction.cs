32fc09344073e7be7c894efe7a039b0753d55c3b
TeBeCo <tebeco@gmail.com>
2026-02-15 00:20:43 +0400

    git init

---
+++ AsyncGoneConflict/HelloWorld/HelloWorldFunction.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace AsyncGoneConflict.HelloWorld;
+
+public class HelloWorldFunction(ILogger<HelloWorldFunction> logger)
+{
+    [Function("hello-world")]
+    public Ok<string> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "hello")] HttpRequestData requestData, FunctionContext _)
+    {
+        logger.LogInformation("", await requestData.Body.ReadAsStringAsync())
+        TypedResults.Ok("hello world !");
+    }
+}
